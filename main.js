21cf67cf2cac060198a4e63d1f3d88e8aacfd8dc
Pedro Rivera <priveraa94@gmail.com>
2024-10-14 09:40:17 -0400

    A simple log in using alerts

---
+++ main.js
@@ -0,0 +1,17 @@
+let userName = prompt('Who\'s there?', '')
+let password = "TheMaster"
+
+if (userName === 'Admin') {
+  let getPassword = prompt('Password: ', '')
+  if (getPassword === password) {
+    alert('Welcome')
+  } else if ((getPassword == '') || (getPassword == null)) {
+    alert('Canceled')
+  } else {
+    alert('Wrong password')
+  }
+} else if ((userName == '') || (userName == null)) {
+  alert('Canceled')
+} else {
+  alert('I don\'t know you')
+}
